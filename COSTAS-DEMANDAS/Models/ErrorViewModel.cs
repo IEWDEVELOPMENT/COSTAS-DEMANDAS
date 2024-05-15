68668eb0d41f090b5555d23267432fd9831e4a62
IEWDEVELOPMENT <analistadesarrollo@iewdevelopment.com.co>
2024-05-15 13:56:12 -0500

    Agregar archivos de proyecto.

---
+++ COSTAS-DEMANDAS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COSTAS_DEMANDAS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
